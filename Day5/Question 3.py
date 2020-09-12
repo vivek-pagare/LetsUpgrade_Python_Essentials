815e10e58fd856aaa0d9b32016249c24119b774a
vivek-pagare <39077108+vivek-pagare@users.noreply.github.com>
2020-09-12 16:52:53 +0530

    Add files via upload

---
+++ Day5/Question 3.py
@@ -0,0 +1,35 @@
+# string = str(input("Enter the sentence: "))
+# print(string)
+
+# demo = lambda str: string.capitalize()
+# ret = demo(str)
+# print("Capitalizing :",ret)
+
+
+
+
+# fp = lambda str : myString.capitalize()
+
+# myString = 'hey this is sai'
+
+# ret = fp( myString)
+# print("Capitalizing :",ret)
+
+# st = "vivek atul akshay"
+
+# ' '.join(list(map(lambda x: x.strip().capitalize(), st.split(' '))))
+
+# print(st)
+
+
+
+# st = "akshay vivek atul"
+
+# st = ' '.join(list(map(lambda x: x.strip().capitalize(), str.split(' '))))
+# print("Capitalizing :",st)
+
+str = "akshay vivek atul abhi"
+
+str = ' '.join(list(map(lambda x: x.strip().capitalize(), str.split(' '))))
+
+print(str)
